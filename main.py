527df3cbd1c19ef963dd0bdc607bf79b2e973ec1
FadilArfat <62172926+FadilArfat@users.noreply.github.com>
2023-02-16 09:29:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify,render_template, request
 import os
+import joblib
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+@app.route('/',methods=['POST', 'GET'])
+def home():
+  feature = open('vect.pkl', 'rb')
+  model = open('iris.pkl', 'rb')
 
+  vec = joblib.load(feature)
+  rbf = joblib.load(model)
+
+  if request.method == 'POST':
+    inp = request.form['inp']
+    data = [inp]
+    vect = vec.transform(data)
+    my_prediction = rbf.predict(vect)[0]
+    if my_prediction == 1:
+        return render_template('home.html', message = "Sentimen negatif 😡")
+    else:
+        return render_template('home.html', message = 'sentimen positif 🙂')
+    
+  return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
